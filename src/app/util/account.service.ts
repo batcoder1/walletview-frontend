8c6556043a5e81f29e54a13c2e3d098324c9c804
wooltar <enrique.rubio.developer@gmail.com>
2019-06-02 13:18:23 +0200

    config env varibles

--- src/app/util/account.service.ts
+++ src/app/util/account.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
  
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-  api_url = process.env.BACKEND;
+  api_url = environment.BACKEND;
   pathBalance = '/balance'
   constructor(  private _http: HttpClient) { }
    
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  BACKEND:'https://walletviewbackend.herokuapp.com/api/v1'
 };
